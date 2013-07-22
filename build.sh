3f35017fbe96a49c952cc7e5ca0338ba83437700
Mike O'Connor <mike@oeg.com.au>
2013-07-22 14:52:50 +0930

    Added a few build options to allow code to build in a mac

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 LATPROCDIR=../latproc
-CFLAGS="-I$LATPROCDIR/include -g"
-c++ -o sampler $CFLAGS -I$LATPROCDIR/iod sampler.cpp -lzmq
+CFLAGS="-I$LATPROCDIR/include -g -I/opt/local/include/ -L/opt/local/lib/"
+c++ -o sampler $CFLAGS -I$LATPROCDIR/iod sampler.cpp -lzmq -lboost_system-mt
 c++ -o scope $CFLAGS scope.cpp
 c++ -o filter $CFLAGS -I$LATPROCDIR/iod filter.cpp $LATPROCDIR/iod/regular_expressions.cpp
