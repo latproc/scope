02e1a19177a77260134fd31cc78913b04f444027
mleadbeater <mleadbeater@me.com>
2013-07-23 05:20:56 +0930

    support optionally republishing messages add a raw mode to sample all messages received through zmq  ( useful for a receiver of another sampler )

--- sampler.cpp
+++ sampler.cpp
@@ -18,6 +18,16 @@
   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
+/*
+ This sampler collects STATE and VALUE changes from a zmq channel and displays
+the received data on stdout with the addition of a timestamp indicating when
+the message was received.
+
+Devices are mapped to a unique integer that is displayed or republished 
+instead of the device name. A second channel is used to transmit new machine names
+as they are discovered. A command channel can be used to request that all 
+device names are resent.
+*/
 #include <iostream>
 #include <iterator>
 #include <stdio.h>
@@ -30,6 +40,7 @@
 #include <stdlib.h>
 #include <fstream>
 #include <signal.h>
+#include <MessagingInterface.h>
 
 using namespace std;
 
@@ -75,7 +86,7 @@ class SamplerOptions {
     bool quiet;
   public:
     SamplerOptions() : subscribe_to_port(5556), subscribe_to_host("localhost"),
-        publish_to_port(5560), publish_to_interface("0.0.0.0"), 
+        publish_to_port(5560), publish_to_interface("*"), 
 		republish(false), quiet(false) {}
 	bool parseCommandLine(int argc, const char *argv[]);
 
@@ -95,9 +106,10 @@ bool SamplerOptions::parseCommandLine(int argc, const char *argv[]) {
         ("republish", "publish received messages")
         ("subscribe", po::value<string>(), "host to subscribe to [localhost]")
         ("subscribe-port", po::value<int>(), "port to subscribe to [5556]")
-        ("interface",po::value<string>(),  "local interface to publish to [0.0.0.0]")
+        ("interface",po::value<string>(),  "local interface to publish to [*]")
         ("publish-port", po::value<int>(), "port to subscribe to [5560]")
 		("quiet", "quiet mode, no output on stdout")
+		("raw", "process all received messages, not just state and value changes")
         ;
         po::variables_map vm;        
         po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -130,6 +142,119 @@ bool SamplerOptions::parseCommandLine(int argc, const char *argv[]) {
 	return true;
 }
 
+
+struct CommandThread {
+    void operator()();
+    CommandThread();
+    void stop();
+    bool done;
+private:
+};
+
+struct Command {
+public:
+    Command()  : done(false), error_str(""), result_str("") {}
+    virtual ~Command(){ }
+    bool done;
+    const char *error() { return error_str.c_str(); }
+    const char *result() { return result_str.c_str(); }
+    
+	bool operator()(std::vector<std::string> &params) {
+		done = run(params);
+		return done;
+	}
+    
+protected:
+	virtual bool run(std::vector<std::string> &params) = 0;
+	std::string error_str;
+	std::string result_str;
+};
+
+struct CommandRefresh : public Command {
+	bool run(std::vector<std::string> &params);
+};
+
+bool CommandRefresh::run(std::vector<std::string> &params) {
+	return false;
+}
+
+void sendMessage(zmq::socket_t &socket, const char *message) {
+    const char *msg = (message) ? message : "";
+    size_t len = strlen(msg);
+    zmq::message_t reply (len);
+    memcpy ((void *) reply.data (), msg, len);
+    socket.send (reply);
+}
+
+
+
+void CommandThread::operator()() {
+    std::cout << "------------------ Command Thread Started -----------------\n";
+    zmq::context_t context (3);
+    zmq::socket_t socket (context, ZMQ_REP);
+    socket.bind ("tcp://*:5555");
+    
+    while (!done) {
+        try {
+            
+             zmq::pollitem_t items[] = { { socket, 0, ZMQ_POLLERR | ZMQ_POLLIN, 0 } };
+             zmq::poll( &items[0], 1, 500*1000);
+             if ( !(items[0].revents & ZMQ_POLLIN) ) continue;
+
+        	zmq::message_t request;
+            if (!socket.recv (&request)) continue; // interrupted system call
+            size_t size = request.size();
+            char *data = (char *)malloc(size+1);
+            memcpy(data, request.data(), size);
+            data[size] = 0;
+            std::cout << "Command thread received " << data << std::endl;
+            std::istringstream iss(data);
+            std::list<std::string> parts;
+            std::string ds;
+			Command *command = 0;
+            int count = 0;
+            while (iss >> ds) {
+                parts.push_back(ds);
+                ++count;
+            }
+            std::vector<std::string> params(0);
+            std::copy(parts.begin(), parts.end(), std::back_inserter(params));
+            if (params.empty()) {
+                sendMessage(socket, "Empty message received\n");
+                goto cleanup;
+            }
+            ds = params[0];
+			if (ds == "") {
+				command = new CommandRefresh();
+			}
+			else {
+				command = new CommandRefresh();
+			}
+            if ((*command)(params)) {
+                //std::cout << command->result() << "\n";
+                sendMessage(socket, command->result());
+            }
+            else {
+                NB_MSG << command->error() << "\n";
+                sendMessage(socket, command->error());
+            }
+            delete command;
+            
+        cleanup:
+            free(data);
+        }
+        catch (std::exception e) {
+			if (errno) std::cout << "error during client communication: " << strerror(errno) << "\n" << std::flush;
+            if (zmq_errno())
+                std::cerr << zmq_strerror(zmq_errno()) << "\n" << std::flush;
+            else
+                std::cerr << " Exception: " << e.what() << "\n" << std::flush;
+			if (zmq_errno() != EINTR && zmq_errno() != EAGAIN) abort();
+        }
+    }
+    socket.close();
+}
+
 int main(int argc, const char * argv[])
 {
 	SamplerOptions options;
@@ -138,24 +263,15 @@ int main(int argc, const char * argv[])
 	if (options.quietMode() && !options.publish())
 		cerr << "Warning: not writing to stdout or zmq\n";
 
+	MessagingInterface *mif = 0;
+    if (options.publish())
+	 	mif = MessagingInterface::create("*", options.publisherPort());
+	
 	atexit(save_devices);
 	atexit(save_state_names);
 	signal(SIGINT, interrupt_handler);
 	signal(SIGTERM, interrupt_handler);
         // this should be a separate thread
-    if (options.publish()) {
-        //server
-        int count = 0;
-        zmq::context_t context (1);
-        zmq::socket_t publisher (context, ZMQ_PUB);
-        publisher.bind("tcp://*:5556");
-        for (;;) {
-            zmq::message_t message(20);
-            snprintf ((char *) message.data(), 20, "%d", ++count);
-            publisher.send(message);
-            sleep(1);
-        }
-    }
     try {
         // client
 		int next_device_num = 0;
@@ -170,6 +286,7 @@ int main(int argc, const char * argv[])
         subscriber.connect(ss.str().c_str());
 		struct timeval start;
 		gettimeofday(&start, 0);
+		stringstream output;
         for (;;) {
             zmq::message_t update;
             subscriber.recv(&update);
@@ -184,6 +301,8 @@ int main(int argc, const char * argv[])
             istringstream iss(data);
             string point, op, state;
             iss >> point >> op;
+			output.str("");	
+			output.clear();
 
             if (op == "STATE") {					
                 iss >> state;
@@ -200,9 +319,9 @@ int main(int argc, const char * argv[])
 				if (idx == device_map.end()) //new machine
 					device_map[point] = next_device_num++;
 				
-				if (!options.quietMode())
-					cout << get_diff_in_microsecs(&now, &start) << "\t" 
-						<< point << "\t" << state << "\t" << state_num<< "\n" << flush;
+				output << get_diff_in_microsecs(&now, &start) << "\t" 
+					<< point << "\t" << state << "\t" << state_num<< "\n";
+				if (!options.quietMode()) cout << output.str() << flush;
             }
 			else if (op == "VALUE") {
 				map<string, int>::iterator idx = device_map.find(point);
@@ -214,10 +333,15 @@ int main(int argc, const char * argv[])
 				long val;
 				char *remainder;
 				val = strtol(s_value.c_str(), &remainder, 0);
-				if (*remainder == 0)
+				if (*remainder == 0) {
+					output << get_diff_in_microsecs(&now, &start) << "\t" 
+							<< point << "\tvalue\t" << val << "\n";
 					if (!options.quietMode()) 
-						cout << get_diff_in_microsecs(&now, &start) << "\t" 
-							<< point << "\tvalue\t" << val << "\n" << flush;
+						cout << output.str() << flush;
+				}
+			}
+			if (options.publish()) {
+				mif->send(output.str().c_str());
 			}
 			delete data;
         }
