c4cd4caf4044a093d621d136a3b2b2c36a306148
Mike O'Connor <mike@oeg.com.au>
2014-01-03 18:03:19 +1030

    Added an option to build with the -mt libs and BOOST

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 LATPROCDIR=../latproc
+BOOST_EXTRA=-mt
 CFLAGS="-I$LATPROCDIR/include -g -I/opt/local/include/ -L/opt/local/lib/"
-c++ -o sampler $CFLAGS -I$LATPROCDIR/iod sampler.cpp -lzmq -lboost_system -lboost_program_options \
+c++ -o sampler $CFLAGS -I$LATPROCDIR/iod sampler.cpp -lzmq -lboost_system${BOOST_EXTRA} -lboost_program_options${BOOST_EXTRA} \
 	$LATPROCDIR/iod/MessagingInterface.cpp $LATPROCDIR/iod/Logger.cpp
 c++ -o scope $CFLAGS scope.cpp
 c++ -o filter $CFLAGS -I$LATPROCDIR/iod filter.cpp $LATPROCDIR/iod/regular_expressions.cpp 
