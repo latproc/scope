ca07a9b5dc1882ee093fea39522e5a9c77e24d3c
mleadbeater <mleadbeater@me.com>
2014-02-23 20:10:19 +1030

    removed continuous operation idea

--- filter.cpp
+++ filter.cpp
@@ -7,8 +7,10 @@ int main(int argc, char *argv[]) {
 	bool continuous = false; // ignore problems reading stdin, loop continuously
 	std::list<rexp_info*>patterns;
 	for (int i=1; i<argc; ++i) {
-		if (strcmp(argv[i],"-c") ==0 )
+		if (strcmp(argv[i],"-c") ==0 ) {
 			continuous = true;
+			continue;
+		}
 		int result = 1;
 		rexp_info *info = create_pattern(argv[i]);
 		if (info->compilation_result == 0)
@@ -21,17 +23,15 @@ int main(int argc, char *argv[]) {
 	
 	char buf[100];
 	
-	do {
-		while (std::cin.getline(buf, 100, '\n') ) {
-			std::list<rexp_info*>::iterator iter = patterns.begin();
-			while (iter != patterns.end()) {
-				rexp_info *info = *iter++;
-				if (execute_pattern(info, buf) == 0) {
-					std::cout << buf << "\n" << std::flush;
-					break;
-				}
+	while (std::cin.getline(buf, 100, '\n') ) {
+		std::list<rexp_info*>::iterator iter = patterns.begin();
+		while (iter != patterns.end()) {
+			rexp_info *info = *iter++;
+			if (execute_pattern(info, buf) == 0) {
+				std::cout << buf << "\n" << std::flush;
+				break;
 			}
 		}
-	} while (continuous);
+	}
 	return 0;
 }
