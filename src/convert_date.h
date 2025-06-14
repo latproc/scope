53fbf3b28eba2a52627e85c6ef4baeec0ee7c01f
Martin Leadbeater <mleadbeater@me.com>
2025-06-14 12:47:02 +0930

    src/convert_date: Change tabs to spaces

--- src/convert_date.h
+++ src/convert_date.h
@@ -2,27 +2,27 @@
 #include <string>
 
 struct DateTime {
-	tm datetime;
-	int frac_sec;
+    tm datetime;
+    int frac_sec;
 };
 
 void display(tm &time, int frac_sec);
 void display(DateTime &datetime);
 
 enum Term {
-	none,
-	year,
-	mon,
-	day,
-	timesep,
-	hour,
-	min,
-	secs,
-	fracsecs,
-	zonesep,
-	zonehr,
-	zhmsep,
-	zonemin
+    none,
+    year,
+    mon,
+    day,
+    timesep,
+    hour,
+    min,
+    secs,
+    fracsecs,
+    zonesep,
+    zonehr,
+    zhmsep,
+    zonemin
 };
 
 Term parse_8601_datetime(const std::string &input, DateTime &result);
